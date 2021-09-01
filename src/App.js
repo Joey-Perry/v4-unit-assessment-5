d03cb61802d3fe99ee1367ed507d7877fcb79bd8
Joey-Perry <Joeyap84@gmail.com>
2021-08-31 20:48:05 -0500

    Added routes to front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import routes from './routes';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'></div>
+    <>
+      <Nav />
+      { routes }
+    </>
   )
 };
 
